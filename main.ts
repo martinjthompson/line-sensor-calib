cb651db29615836c456aff5cc42a0fd13dd513c6
Martin Thompson <martin@parallelpoints.com>
2020-07-17 17:11:04 +0100

    Distance -> LEDs and serial

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
+let brightness = 0
+let minimumDistance = 0
+let maximumDistance = 0
+let distance = 0
 let moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
-moveMotorZIP.setZipLedColor(0, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Yellow))
+moveMotorZIP.setBrightness(255)
+moveMotorZIP.showColor(Kitronik_Move_Motor.rgb(0, 0, 0))
+Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
+Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
 basic.forever(function () {
-    control.waitMicros(400000)
-    Kitronik_Move_Motor.soundSiren(Kitronik_Move_Motor.OnOffSelection.On)
-    control.waitMicros(400000)
-    Kitronik_Move_Motor.soundSiren(Kitronik_Move_Motor.OnOffSelection.Off)
+    serial.writeNumber(distance)
+    serial.writeString(",")
+    serial.writeLine("")
+    control.waitMicros(100000)
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    maximumDistance = 25
+    minimumDistance = 0
+    distance = Kitronik_Move_Motor.measure()
+    brightness = 255 - (distance - minimumDistance) / (maximumDistance - minimumDistance) * 255
+    if (distance < maximumDistance) {
+        moveMotorZIP.showColor(Kitronik_Move_Motor.rgb(brightness, 0, 0))
+    } else {
+        moveMotorZIP.showColor(Kitronik_Move_Motor.rgb(0, 0, 0))
+    }
 })
